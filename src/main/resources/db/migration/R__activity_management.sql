a4c3fa03c485e0bc7298a814c410fdfb360de2ad
Maël Le Folgoc Pontis <maellefolgocpontis@yahoo.fr>
2019-05-22 14:12:22 +0200

    GIT Fix 8.1.2 Project compiles.

--- src/main/resources/db/migration/R__activity_management.sql
+++ src/main/resources/db/migration/R__activity_management.sql
@@ -1,36 +1,35 @@
-CREATE OR REPLACE FUNCTION add_activity(in_title varchar, in_description text, in_owner_id bigint DEFAULT null) RETURNS activity AS $$
+--
+-- add an activity
+--
+CREATE OR REPLACE FUNCTION add_activity(in_title varchar(500),in_description text, in_owner_id bigint default null) RETURNS activity AS $$
+    DECLARE
+        default_owner "user"%rowtype;
+        activity_id bigint;
+        activity_res activity%rowtype;
+    BEGIN
+        if in_owner_id is null then
+            default_owner := get_default_owner();
+            in_owner_id := default_owner.id;
+        end if;
+        activity_id := nextval('id_generator');
+        insert into activity (id, title, description,creation_date, modification_date, owner_id)
+            values(activity_id, in_title, in_description,now(), now(), in_owner_id);
+        select * into activity_res
+            from activity
+            where id = activity_id;
+        return activity_res;
+    END;
+$$ language plpgsql;
 
-	DECLARE
-	
-		line activity%ROWTYPE;
-	
-	BEGIN
-	
-		IF in_owner_id IS NULL THEN
-			SELECT id INTO in_owner_id FROM "user" WHERE username = 'Default Owner';
-		END IF;
-	
-		INSERT INTO activity (id, title, description, owner_id)
-		VALUES (nextval('id_generator'), in_title, in_description, in_owner_id)
-		RETURNING * INTO line;
-		
-		RETURN line;
-	
-	END;
-
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION find_all_activities(activities_cursor refcursor) RETURNS refcursor AS $$
-
-	BEGIN
-	
-		OPEN activities_cursor FOR SELECT activity.id, activity.title, "user".username
-								   FROM activity
-					               LEFT JOIN "user" ON "user".id = activity.owner_id
-					               ORDER BY title ASC, "user".username ASC;
-					    
-		RETURN activities_cursor;
-		
-	END;
-
-$$ LANGUAGE plpgsql;
+--
+-- find all activities (version param OUT)
+--
+CREATE OR REPLACE FUNCTION find_all_activities(OUT activities_curs refcursor) AS $$
+    begin
+     open activities_curs FOR
+            select act.id as id, title, description, creation_date, modification_date, owner_id, username
+                from activity act left join "user" owner
+                on act.owner_id = owner.id
+                order by title, username;
+    END;
+$$ language plpgsql;
